7dc63bc08dba565a0a2da3b8d48e9591eed2cce2
tommy <torreslicet008@gmail.com>
2024-05-14 11:38:44 -0700

    fixed audio issue

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -8,33 +8,36 @@ public class PlayerCharacter : MonoBehaviour
     //players health variables
     public int maxHealth = 20; 
     public int playerHealth;
-
-    public float elapsedTime = 0f; // to track elapsed time
+    public AudioSource audioSource; // Attach the AudioSource component
+    public AudioClip hurtSound; 
 
     // Start is called before the first frame update
     void Start()
     {
         //set max health to player health
         playerHealth = maxHealth;
-        elapsedTime = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        // Debug.Log($"Elapsed Time: {elapsedTime} seconds");
+        SessionData.ElapsedTime += Time.deltaTime;
+        Debug.Log($"Elapsed Time: {SessionData.ElapsedTime} seconds");
     }
 
     // taking damage
     public float GetElapsedTime(){
-        return elapsedTime;
+        return SessionData.ElapsedTime;
     }
     public void Hurt(int damage)
     {
         playerHealth -= damage;
         Debug.Log($"Helath: {playerHealth}");
         //Player Dies go to Main menu
+        if (damage > 0)
+        {
+            audioSource.PlayOneShot(hurtSound);
+        }
         if(playerHealth <= 0){
             Debug.Log("YOU DIED");
             //add return to main menu
